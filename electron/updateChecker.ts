387dff2bc0214f0bc1fe261603e68de374200d88
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2023-12-28 20:59:16 +0100

    chore: remove unused variables

--- electron/updateChecker.ts
+++ electron/updateChecker.ts
@@ -1,23 +1,8 @@
 import { app } from "electron"
-import getPlatform from "./utils/getPlatform.js"
 
 export default async function updateChecker() {
-    const currentOS = getPlatform()
     const getLatest = await fetch('https://api.github.com/repos/sern-handler/gui/releases/latest')
         .then(res => res.json())
-    
-    const dlUrl = getLatest.assets.map((asset) => {
-        switch (asset.content_type) {
-            case 'application/x-msdownload':
-                return 'windows'
-            case 'application/vnd.appimage':
-                return 'linux'
-            case 'application/x-apple-diskimage':
-                return 'macOS'
-            default:
-                return null
-        }
-    })
 
     if (`v${app.getVersion()}` !== getLatest.tag_name) {
         return { version: getLatest.tag_name as string, url: getLatest.html_url as string }
